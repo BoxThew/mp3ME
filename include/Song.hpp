c6ff31a31c9488f752c659438a31a819cf22cdc1
BoxThew <rezerotart@gmail.com>
2025-12-04 17:34:46 -0800

    added genre vec to Songs

--- include/Song.hpp
+++ include/Song.hpp
@@ -1,12 +1,14 @@
 #ifndef SONG_HPP
 #define SONG_HPP
 #include <string>
+#include <vector>
 
 class Song{
     std::string title;
     std::string artist;
     std::string album;
     unsigned int year_rel;
+    std::vector<std::string> genres;
 
 public:
     Song();
@@ -15,11 +17,14 @@ public:
     void set_artist(const std::string& artist);
     void set_album(const std::string& album);
     void set_year_rel(const int year_rel);
+    void add_genre(const std::string& genre);
+
 
     std::string get_title();
     std::string get_artist();
     std::string get_album();
     unsigned int get_year_rel();
+    std::vector<std::string> get_genres();
 
 };
 
--- src/Song.cpp
+++ src/Song.cpp
@@ -1,5 +1,7 @@
 #include "Song.hpp"
 #include <string>
+#include <vector>
+
 
 Song::Song(){
 
@@ -18,9 +20,13 @@ void Song::set_album(const std::string& album){
 void Song::set_year_rel(const int year_rel){
     this->year_rel = year_rel;
 }
+void Song::add_genre(const std::string& genre){
+    this->genres.push_back(genre);
+}
 
 
 std::string Song::get_title(){return title;}
 std::string Song::get_artist(){return artist;}
 std::string Song::get_album(){return album;}
 unsigned int Song::get_year_rel(){return year_rel;}
+std::vector<std::string> Song::get_genres(){return genres}
